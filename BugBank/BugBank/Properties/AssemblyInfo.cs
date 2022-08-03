325aa2f1e43c59b4cd48dc0d127e8eba0556259c
Mikael Italo <mikael.italo.35@hotmail.com>
2022-08-02 23:59:56 -0300

    Add project files.

---
+++ BugBank/BugBank/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BugBank")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BugBank")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("989b7069-9cec-492a-9b7d-765cbace3599")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SwagLabs/SwagLabs/Utils.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabs
+{
+    internal class Utils
+    {
+
+        #region Browser
+
+        public static IWebDriver getBrowserLocal(IWebDriver driver, String browser)
+        {
+            switch (browser)
+            {
+                case "Internet Explorer":
+                    driver = new OpenQA.Selenium.IE.InternetExplorerDriver();
+                    driver.Manage().Window.Maximize();
+                    break;
+
+                case "Chrome":
+                    driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+                    driver.Manage().Window.Maximize();
+                    break;
+
+                default:
+                    driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+                    driver.Manage().Window.Maximize();
+                    break ;
+
+            }
+            return driver;
+        }
+
+
+        public static IWebDriver getBrowserRemote(IWebDriver driver, String uri)
+        {
+            FirefoxOptions cap_firefox = new FirefoxOptions();
+            //ChromeOptions cap_chrome = new ChromeOptions();
+            driver = new RemoteWebDriver(new Uri(uri), cap_firefox);
+
+            return driver;
+        }
+
+        #endregion
+
+
+        #region JavaScript
+        public static void executeJS(IWebDriver driver, String script)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor) driver;
+            js.ExecuteScript(script);
+        }
+        #endregion
+
+        #region Espera
+        public void EsperaExplicita(IWebDriver driver, String Xpath)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath(Xpath)));
+        }
+        #endregion
+    }
+}
